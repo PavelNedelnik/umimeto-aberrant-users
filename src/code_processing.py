c0de4753d9ef8995dbfc0d78b93d5d927acf14a6
nedelnik <p.nedelnik@email.cz>
2024-04-30 09:25:53 +0200

    WIP

--- src/code_processing.py
+++ src/code_processing.py
@@ -1,71 +1,70 @@
-import urllib
-import re
 import ast
+import re
+import urllib
 from base64 import b64decode
+
 from unidecode import unidecode
 
 
 def decode_code_string(code: str) -> str:
     """
-    Decode and standardize base64 encoded python code. Main endpoint for the module.
+    Decode and standardize base64 encoded python code.
     """
     return unidecode(clean_code(parse_code(code)))
 
 
 def parse_code(encoded_field, raise_error=False):
-    """Parse url encoded, base64 encoded python code.
-    """
+    """Parse url encoded, base64 encoded python code."""
     try:
-        unquoted = urllib.parse.unquote(encoded_field, errors='strict')
-    except ValueError as exc:
+        unquoted = urllib.parse.unquote(encoded_field, errors="strict")
+    except ValueError:
         if raise_error:
-            raise            
-        return 'INVALID: url-unquoting'
-    
-    try: 
-        code = b64decode(unquoted, altchars='  ').decode('utf-8', errors='strict')      
-        #code = um.utils.code_processing.decode_program(unquoted)
-    except ValueError as exc:
+            raise
+        return "INVALID: url-unquoting"
+
+    try:
+        code = b64decode(unquoted, altchars="  ").decode("utf-8", errors="strict")
+        # code = um.utils.code_processing.decode_program(unquoted)
+    except ValueError:
         if raise_error:
-            raise 
-        return 'INVALID: b64-decoding'
+            raise
+        return "INVALID: b64-decoding"
 
     return code.strip()
 
 
 def clean_code(code, raise_error=False):
-    """Fix some quirks allowed by the online python interpreter.
-    """
+    """Fix some quirks allowed by the online python interpreter."""
     code = fix_indent(code, raise_error=raise_error)
-    if code.startswith('INVALID'):
+    if code.startswith("INVALID"):
         return code
     err = get_parse_error(code)
     if not err:
         return code
-    if 'leading zeros' in str(err):
-        code = re.sub(r'0+(\d+)', r'\1', code)
-    if '<>' in code:
-        code = code.replace('<>', '!=')
+    if "leading zeros" in str(err):
+        code = re.sub(r"0+(\d+)", r"\1", code)
+    if "<>" in code:
+        code = code.replace("<>", "!=")
     if is_valid_python(code):
         return code
     if raise_error:
-        raise ValueError(f'{err}\n{code}')
-    return 'INVALID: syntax'
+        raise ValueError(f"{err}\n{code}")
+    return "INVALID: syntax"
 
 
 def fix_indent(code, raise_error=False):
     if valid_indent(code):
         return code
     for spaces_per_tab in [8, 4]:
-        code2 = code.replace('\t', ' ' * spaces_per_tab)
+        code2 = code.replace("\t", " " * spaces_per_tab)
         # It's not sufficient to ask for valid indent, since
-        # the attempted fix may lead to correct indent but 
+        # the attempted fix may lead to correct indent but
         # a syntax error (e.g., else clause too deep).
         if is_valid_python(code2):
             return code2
     if raise_error:
-        raise ValueError(f'Invalid indent: {code}')
-    return f'INVALID: indent'
+        raise ValueError(f"Invalid indent: {code}")
+    return f"Invalid indent: {code}"
 
 
 def valid_indent(code):
@@ -80,7 +79,7 @@ def get_parse_error(code):
     except SyntaxError as err:
         return err
 
-    
+
 def is_valid_python(code):
     try:
         ast.parse(code)
@@ -90,18 +89,19 @@ def is_valid_python(code):
 
 
 def debug_invalid(submits):
-    mask = submits.code.str.startswith('INVALID')
+    mask = submits.code.str.startswith("INVALID")
     invalid_submits = submits[mask]
     n_invalid = mask.sum()
     p_invalid = mask.mean()
-    print(f'invalid programs: {n_invalid}/{len(submits)} ({p_invalid:.2%})\n')
+    print(f"invalid programs: {n_invalid}/{len(submits)} ({p_invalid:.2%})\n")
     print(invalid_submits.code.value_counts())
-    #display(invalid_submits)    
+    # display(invalid_submits)
     return submits
 
 
 def filter_valid(submits, verbose=True):
     if verbose:
-        print('Filtering invalid programs.')
+        print("Filtering invalid programs.")
         debug_invalid(submits)
+    return submits.query("~code.str.startswith('INVALID')")
     return submits.query("~code.str.startswith('INVALID')")
--- src/load_scripts.py
+++ src/load_scripts.py
@@ -1,10 +1,12 @@
-import pandas as pd
-import numpy as np
-import re
 import json
+import re
 from pathlib import Path
-from src.code_processing import decode_code_string
+
+import numpy as np
+import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+
+from src.code_processing import decode_code_string
 
 
 def load_ipython_item(data_path: Path) -> pd.DataFrame:
@@ -18,74 +20,92 @@ def load_ipython_item(data_path: Path) -> pd.DataFrame:
     """
     # load
     if data_path.is_dir():
-        data_path = data_path / 'umimeprogramovatcz-ipython_item.csv'
-    item = pd.read_csv(data_path, sep=';', index_col=0)
+        data_path = data_path / "umimeprogramovatcz-ipython_item.csv"
+    item = pd.read_csv(data_path, sep=";", index_col=0)
     # process
-    item = item[['name', 'instructions', 'solution']]
-    item['instructions'] = item['instructions'].apply(lambda x: eval(x)[0][1])
-    item['solution'] = item['solution'].apply(lambda x: eval(x)[0][1]).apply(decode_code_string)
-    item = pd.concat([
-        item,
-        *[
-            pd.DataFrame({'name': f'unknown_{i}', 'solution': 'pass', 'instructions': 'unknown'}, index=[idx])
-                for i, idx in enumerate([12, 118, 142, 143, 144, 145, 146])
+    item = item[["name", "instructions", "solution"]]
+    item["instructions"] = item["instructions"].apply(lambda x: eval(x)[0][1])
+    item["solution"] = (
+        item["solution"].apply(lambda x: eval(x)[0][1]).apply(decode_code_string)
+    )
+    item = pd.concat(
+        [
+            item,
+            *[
+                pd.DataFrame(
+                    {
+                        "name": f"unknown_{i}",
+                        "solution": "pass",
+                        "instructions": "unknown",
+                    },
+                    index=[idx],
+                )
+                for i, idx in enumerate([12, 118])
+            ],
         ]
-    ])
+    )
     return item
 
 
-def filter_for_only_codes(linter_messages:list[list[str]], pattern: str=r'^[A-Z0-9]+$') -> list[list[str]]:
+def filter_for_only_codes(
+    linter_messages: list[list[str]], pattern: str = r"^[A-Z0-9]+$"
+) -> list[list[str]]:
     filtered_lists = []
     regex = re.compile(pattern)
-    
+
     for messages in linter_messages:
         messages = [error_code for error_code in messages if regex.match(error_code)]
         filtered_lists.append(messages)
-    
+
     return filtered_lists
 
 
-def load_ipython_log(data_path: Path, linter_messages_path:Path=None) -> pd.DataFrame:
+def load_ipython_log(
+    data_path: Path, linter_messages_path: Path = None
+) -> pd.DataFrame:
     """Load and clean umimeprogramovat ipython log.
 
     Args:
         data_path (Path): Path to the folder with the dataset or the ipython item file.
-        linter_messages_path (Path, optional): Path to the folder with the already generated linter 
+        linter_messages_path (Path, optional): Path to the folder with the already generated linter
             messages. Use None to get the data without the messages.
 
     Returns:
         pd.DataFrame: Loaded and cleaned log.
     """
     if data_path.is_dir():
-        data_path = data_path / 'umimeprogramovatcz-ipython_log.csv'
+        data_path = data_path / "umimeprogramovatcz-ipython_log.csv"
     # load
-    log = pd.read_csv(data_path, sep=';')
+    log = pd.read_csv(data_path, sep=";")
     # process
     log.drop_duplicates(inplace=True)
     log.dropna(inplace=True)
-    log['time'] = pd.to_datetime(log['time'])
-    log['answer'] = log['answer'].apply(decode_code_string)
-    log = log[log['answer'].str.strip().astype(bool)]
+    log["time"] = pd.to_datetime(log["time"])
+    log["answer"] = log["answer"].apply(decode_code_string)
+    log["correct"] = log["correct"].astype(bool)
+    log = log[log["answer"].str.strip().astype(bool)]
 
     if linter_messages_path is not None:
         new_log = []
-        counts = log['user'].value_counts()
+        counts = log["user"].value_counts()
 
         for file_path in (linter_messages_path).glob("edulint_results*.json"):
-            start, end = file_path.stem.split('_')[2].split('-')
+            start, end = file_path.stem.split("_")[2].split("-")
             start, end = int(start), int(end)
             selected_counts = counts[(counts >= start) & (counts < end)]  # noqa: F841
-            log_slice = pd.DataFrame(log.query('user in @selected_counts.index'))
-            messages = filter_for_only_codes(json.load(open(file_path, 'r')))
-            log_slice['linter_messages'] = [' '.join(alist) for alist in messages]
+            log_slice = pd.DataFrame(log.query("user in @selected_counts.index"))
+            messages = filter_for_only_codes(json.load(open(file_path, "r")))
+            log_slice["linter_messages"] = [" ".join(alist) for alist in messages]
             new_log.append(log_slice)
 
         new_log = pd.concat(new_log)
-        vectorizer = CountVectorizer(stop_words = ['f821'], min_df=0.0005, max_df=0.4, binary=True)
-        vectors = vectorizer.fit_transform(new_log['linter_messages'])
+        vectorizer = CountVectorizer(
+            stop_words=["f821"], min_df=0.0005, max_df=0.4, binary=True
+        )
+        vectors = vectorizer.fit_transform(new_log["linter_messages"])
         features = vectorizer.get_feature_names_out()
 
-        new_log['linter_messages'] = list(map(np.array, vectors.toarray().tolist()))
+        new_log["linter_messages"] = list(map(np.array, vectors.toarray().tolist()))
 
         return new_log, features
 
@@ -105,4 +125,5 @@ def load_ipython_log(data_path: Path, linter_messages_path:Path=None) -> pd.Data
         log = pd.concat(new_log)
     """
 
+    return log
     return log
